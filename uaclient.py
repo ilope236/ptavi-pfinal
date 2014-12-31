3d9a40ad841726f0ae06b439bded7f5220050a25
Irene Lope <irenelope236@hotmail.com>
2014-12-31 15:41:29 +0100

    Handler XML

--- uaclient.py
+++ uaclient.py
@@ -6,63 +6,136 @@ Programa cliente que abre un socket a un servidor
 
 import socket
 import sys
+import os
+
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 
 # Cliente UDP simple.
 
 #Comprobamos errores en los datos
-metodos = ('INVITE', 'BYE')
+metodos = ('INVITE', 'BYE', 'REGISTER')
 
 try:
-    METODO = sys.argv[1]
-    USER = sys.argv[2].split('@')[0]
-    IP = sys.argv[2].split('@')[1].split(':')[0]
-    PORT = int(sys.argv[2].split(':')[1])
-    if METODO not in metodos:
-        print 'Usage: python client.py method receiver@IP:SIPport'
-        raise SystemExit
+	CONFIG = sys.argv[1]
+	METODO = sys.argv[2]
+	OPCION = sys.argv[3]
+	if METODO not in metodos:
+		print 'Usage: python uaclient.py config method option'
+		raise SystemExit
 except IndexError:
-    print 'Usage: python client.py method receiver@IP:SIPport'
-    raise SystemExit
+	print 'Usage1: python uaclient.py config method option'
+	raise SystemExit
 except ValueError:
-    print 'Usage: python client.py method receiver@IP:SIPport'
-    raise SystemExit
+	print 'Usage2: python uaclient.py config method option'
+	raise SystemExit
 
 
-def enviar_peticion(metodo):
+class XMLHandler(ContentHandler):
     """
-    Función que crea las peticiones y las envía
+    Handler de XML
     """
-    peticion = metodo + ' sip:' + USER + '@' + IP + ' SIP/2.0\r\n\r\n'
+
+    def __init__(self):
+        """
+        Constructor, creamos las variables
+        """
+        self.lista_dic = []
+        self.tags = ['account', 'uaserver', 'rtpaudio', 'regproxy', 'log', 'audio']
+        self.attrs = {
+            'account': ['username', 'passwd'],
+            'uaserver': ['ip', 'puerto'],
+            'rtpaudio': ['puerto'],
+            'regproxy': ['ip', 'puerto'],
+            'log': ['path'],
+            'audio': ['path']
+        }
+
+    def startElement(self, name, attrs):
+        """
+        Función que se llama al abrir una etiqueta
+        """
+        dic_attrs = {}
+        if name in self.tags:
+            dic_attrs['name'] = name
+            for atributo in self.attrs[name]:
+                dic_attrs[atributo] = attrs.get(atributo, "")
+                #Guardamos en una lista los diccionarios de atributos
+                self.lista_dic.append(dic_attrs)
+
+    def get_tags(self):
+        """
+        Función con la que se obtiene la lista de diccionarios de atributos
+        """
+        return self.lista_dic
+
+
+if __name__ == "__main__":
+
+    parser = make_parser()
+    xHandler = XMLHandler()
+    parser.setContentHandler(xHandler)
+    #Comprobamos que el fichero .xml es válido
+    try:
+        parser.parse(open(CONFIG))
+    except:
+        print 'Usage3: python uaclient.py config method option'
+        raise SystemExit
+
+    #Obtenemos los datos de la configuracion
+    ip = 'localhost'
+    port = 1111
+    username = 'irene@yo.com'
+
+    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.connect((ip, port))
+
+    #Comprobamos el metodo para crear la peticion
+    if METODO == 'REGISTER':
+
+        #Creamos la peticion REGISTER
+        peticion = METODO + ' sip:' + username + ':' + str(port) + ' SIP/2.0\r\n'
+        cabecera = 'Expires: ' + OPCION + '\r\n\r\n'
+        peticion = peticion + cabecera
+    '''	
+    elif METODO == INVITE:
+
+    elif METODO == BYE
+    '''
+    #def enviar_peticion(metodo):
+        #"""
+        #Función que crea las peticiones y las envía
+        #"""
+        #peticion = metodo + ' sip:' + USER + '@' + IP + ' SIP/2.0\r\n\r\n'
+        #print "Enviando: " + peticion
+        #my_socket.send(peticion)
+
+    # Enviamos la peticion
+    #enviar_peticion(METODO)
     print "Enviando: " + peticion
     my_socket.send(peticion)
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((IP, PORT))
+    #Comprobamos que se escucha en el servidor
+    try:
+        data = my_socket.recv(1024)
+    except socket.error:
+        print 'Error: No server listening at ' + ip + ' port ' + str(port)
+        raise SystemExit
 
-# Enviamos la peticion
-enviar_peticion(METODO)
+    print 'Recibido -- ', data
 
-#Comprobamos que se escucha en el servidor
-try:
-    data = my_socket.recv(1024)
-except socket.error:
-    print 'Error: No server listening at ' + IP + ' port ' + str(PORT)
-    raise SystemExit
+    data = data.split('\r\n\r\n')
 
-print 'Recibido -- ', data
+    #Comprobamos que han llegado todos los mensajes
+    if data[0] == 'SIP/2.0 100 Trying':
+        if data[1] == 'SIP/2.0 180 Ringing':
+	        if data[2] == 'SIP/2.0 200 OK':
+		        #Enviamos ACK
+		        enviar_peticion('ACK')
 
-data = data.split('\r\n\r\n')
+    # Cerramos todo
 
-#Comprobamos que han llegado todos los mensajes
-if data[0] == 'SIP/2.0 100 Trying':
-    if data[1] == 'SIP/2.0 180 Ringing':
-        if data[2] == 'SIP/2.0 200 OK':
-            #Enviamos ACK
-            enviar_peticion('ACK')
-
-# Cerramos todo
-
-my_socket.close()
-print "Fin."
+    my_socket.close()
+    print "Fin."
