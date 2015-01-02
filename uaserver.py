63e0aeaff30ad54d20e22c668423bcf555bc9681
Irene Lope <irenelope236@hotmail.com>
2015-01-02 22:34:12 +0100

    Obtencion de sdp del INVITE

--- uaserver.py
+++ uaserver.py
@@ -17,12 +17,11 @@ except IndexError:
     raise SystemExit
 
 metodos = ('INVITE', 'BYE', 'ACK')
-aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 < ' + 'cancion.mp3'
+dic_sdp = {}
 
-
-class XMLHandler(ContentHandler):
+class XMLHandlerUA(ContentHandler):
     """
-    Handler de XML
+    Handler de XML de los User Agent
     """
 
     def __init__(self):
@@ -65,40 +64,50 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
     """
 
     def handle(self):
+        print self.client_address
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
-            peticion = self.rfile.read()
+            line = self.rfile.read()
 
             # Si no hay más líneas salimos del bucle infinito
-            if not peticion:
+            if not line:
                 break
-            print "El cliente nos manda: " + peticion
+            print "El cliente nos manda:\r\n" + line
 
+            peticion = line.split()
             #Obtenemos el método del cliente
-            metodo = peticion.split()[0]
+            metodo = peticion[0]
 
             #Esta en mis metodos?
             if metodo not in metodos:
                 self.wfile.write('SIP/2.0 400 Method Not Allowed\r\n\r\n')
                 print 'Enviamos: SIP/2.0 400 Method Not Allowed\r\n\r\n'
             else:
-                #La petición sigue el estándar SIP?
-                peticion = peticion.split()
                 sip = peticion[1][:4]
                 version = peticion[2]
-                logip = peticion[1]
-                if sip == 'sip:' and '@' in logip and version == 'SIP/2.0':
+                user = peticion[1][4:]
+                if sip == 'sip:' and '@' in user and version == 'SIP/2.0':
                     if metodo == 'INVITE':
+                    
+                        #Guardamos los datos del sdp
+                        peticion = line.split('\r\n\r\n')
+                        sdp = peticion[1].split('\r\n')
+                        for parametro in sdp:
+                            key = parametro.split('=')[0]
+                            dic_sdp[key] = parametro.split('=')[1]
+
                         self.wfile.write('SIP/2.0 100 Trying\r\n\r\n'
                                          + 'SIP/2.0 180 Ringing\r\n\r\n'
                                          + 'SIP/2.0 200 OK\r\n\r\n')
-                        print ('Enviamos: SIP/2.0 100 Trying\r\n\r\n'
+                        print ('\r\nEnviamos: SIP/2.0 100 Trying\r\n\r\n'
                                + 'Enviamos: SIP/2.0 180 Ringing\r\n\r\n'
                                + 'Enviamos: SIP/2.0 200 OK\r\n\r\n')
+                               
                     elif metodo == 'BYE':
                         self.wfile.write('SIP/2.0 200 OK\r\n\r\n')
                         print 'Enviamos: SIP/2.0 200 OK\r\n\r\n'
                     elif metodo == 'ACK':
+                        
                         print 'Vamos a ejecutar', aEjecutar
                         os.system(aEjecutar)
                         print 'Ha terminado la cancion\r\n'
@@ -109,7 +118,7 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 if __name__ == "__main__":
 
     parser = make_parser()
-    xHandler = XMLHandler()
+    xHandler = XMLHandlerUA()
     parser.setContentHandler(xHandler)
     #Comprobamos que el fichero .xml es válido
     try:
@@ -122,28 +131,21 @@ if __name__ == "__main__":
     for dicc in xHandler.lista_dic:
         if dicc['tag'] == 'account':
             username = dicc['username']
-            print 'username => ' + username
             passwd = dicc['passwd']
-            print 'password => ' + passwd
         elif dicc['tag'] == 'uaserver':
             ip_server = dicc['ip']
-            print 'ip_server => ' + ip_server
+            if ip_server == "":
+                ip_server = '127.0.0.1'
             port_server = int(dicc['puerto'])
-            print 'puerto_server => ' + str(port_server)
         elif dicc['tag'] == 'rtpaudio':
             port_rtp = int(dicc['puerto'])
-            print 'puerto_rtp => ' + str(port_rtp)
         elif dicc['tag'] == 'regproxy':
             ip_pr = dicc['ip']
-            print 'ip_pr => ' + ip_pr
             port_pr = int(dicc['puerto'])
-            print 'puerto_proxy => ' + str(port_pr)
         elif dicc['tag'] == 'log':
             path_log = dicc['path']
-            print 'log => ' + path_log
         elif dicc['tag'] == 'audio':
             path_audio = dicc['path']
-            print 'audio => ' + path_audio
 	    
     # Creamos servidor de eco y escuchamos
     serv = SocketServer.UDPServer(("", port_server), EchoHandler)
